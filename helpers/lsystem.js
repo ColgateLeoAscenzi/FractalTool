88ed976f18a7a8cbcd6fa904e068e82ead27e54b
ColgateLeoAscenzi <lascenzi@colgate.edu>
2019-10-28 14:42:46 -0400

    reorganized code

---
+++ helpers/lsystem.js
@@ -0,0 +1,14 @@
+
+var ABrule1 = {
+	"in": "A",
+	"out": "AB"
+}
+var ABrule2 = {
+	"in": "B",
+	"out": "A"
+}
+var ABLsystem = {
+	"rules": [ABrule1, ABrule2],
+	"axiom": "A",
+	"alphabet": ["A","B"]
+}
