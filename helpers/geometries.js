ac3e81b00bf9a50775c1debd911eccea9c4f744f
ColgateLeoAscenzi <lascenzi@colgate.edu>
2019-10-28 14:58:35 -0400

    added dynamic rules stage 1, display

--- helpers/geometries.js
+++ helpers/geometries.js
@@ -5,56 +5,17 @@ var word = []
 
 
 function doLSystem(){
-	word = [ABLsystem.axiom];
+	word = [Lsystem.axiom];
 	generateIteration();
 	
 }
 
-function generateIteration(){
-	var addDiv = document.createElement("div");
-	addDiv.id = "gen"+generation;
-	if(generation == 0){
-		addDiv.append(ABLsystem.axiom);
-		document.getElementById("string-container").append(addDiv);
-		generation++;
-	}
-	else{
-		word = nextGeneration();
-		addDiv.append(word);
-		document.getElementById("string-container").append(addDiv);
-		generation++;
-	}
-
-	
-}
-
-function resetIteration(){
-	  var bodyObj = document.getElementById("string-container");
-	  while (bodyObj.firstChild) {
-		bodyObj.removeChild(bodyObj.firstChild);
-	  }
-	word = ABLsystem.axiom;
-	generation=0;
-	
-}
-
-function nextGeneration(){
-	var outWord = ""
-	for(var i = 0; i < word.length; i++){
-		var currChar = word[i];
-		for(var j = 0; j < ABLsystem.rules.length;j++){
-			if(ABLsystem.rules[j].in == currChar){
-				outWord += ABLsystem.rules[j].out;
-			}
-		}
-	}
-	return outWord
-}
 
 function drawFractalDemo(){
     pushTransform();
-        transform.translate(-0.5,-0.4);
-        filledTriangle(concatColor([colors[0],colors[1],colors[2]]), genTriangle(defCenter, defSideLen, [angles[0], angles[1], angles[2]]));
+//        transform.translate(-0.5,-0.4);
+//        filledTriangle(concatColor([colors[0],colors[1],colors[2]]), genTriangle(defCenter, defSideLen, [angles[0], angles[1], angles[2]]));
+	
     popTransform();
 }
 
--- helpers/lsystem.js
+++ helpers/lsystem.js
@@ -1,14 +1,72 @@
 
-var ABrule1 = {
-	"in": "A",
-	"out": "AB"
+var rule1 = {
+	"in": "1",
+	"out": "1"
 }
-var ABrule2 = {
-	"in": "B",
-	"out": "A"
+var rule2 = {
+	"in": "0",
+	"out": "1[0]0"
 }
-var ABLsystem = {
-	"rules": [ABrule1, ABrule2],
-	"axiom": "A",
-	"alphabet": ["A","B"]
+var Lsystem = {
+	"rules": [rule1, rule2],
+	"axiom": "0",
+	"alphabet": ["0","1","[","]"]
+}
+
+
+function generateIteration(){
+	var addDiv = document.createElement("div");
+	addDiv.id = "gen"+generation;
+	if(generation == 0){
+		addDiv.append(Lsystem.axiom);
+		document.getElementById("string-container").append(addDiv);
+		generation++;
+	}
+	else{
+		word = nextGeneration();
+		addDiv.append(word);
+		document.getElementById("string-container").append(addDiv);
+		generation++;
+	}
+
+	
+}
+
+function resetIteration(){
+	  var bodyObj = document.getElementById("string-container");
+	  while (bodyObj.firstChild) {
+		bodyObj.removeChild(bodyObj.firstChild);
+	  }
+	word = Lsystem.axiom;
+	generation=0;
+	
+}
+
+function nextGeneration(){
+	var outWord = ""
+	for(var i = 0; i < word.length; i++){
+		var currChar = word[i];
+		for(var j = 0; j < Lsystem.rules.length;j++){
+			if(Lsystem.rules[j].in == currChar){
+				outWord += Lsystem.rules[j].out;
+			}
+		}
+	}
+	return outWord
+}
+
+function submitRules(){
+	var rules = "";
+	for(var i = 0; i < Lsystem.rules.length; i++){
+		if(i == Lsystem.rules.length-1){
+			rules += Lsystem.rules[i].in + " -> " + Lsystem.rules[i].out
+		}
+		else{
+			rules += Lsystem.rules[i].in + " -> " + Lsystem.rules[i].out+ ", "
+		}
+		
+		
+	}
+	document.getElementById("rules").innerHTML = "Alphabet = ("+ Lsystem.alphabet + ")<br>Rules: ("+rules+")<br>Axiom: ("+Lsystem.axiom+") "; 
+	
 }
